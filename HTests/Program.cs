444c2fa6243b4fa17e18702a8ef1ef78a57720f2
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-11-09 07:42:01 +0330

    :(

--- HTests/Program.cs
+++ HTests/Program.cs
@@ -10,26 +10,6 @@ namespace HTests
     {
         static void Main(string[] args)
         {
-            var tick = 1000_000;
-            Stopwatch s = new Stopwatch();
-            s.Start();
-            var p = new BigInteger(Int256.MaxValue.ToByteArray());
-            for (int i = 0; i < tick; i++)
-            {
-                p--;
-            }
-            s.Stop();
-            Console.WriteLine(s.ElapsedMilliseconds);
-            s.Reset();
-            s.Start();
-            Int256 pp = Int256.MaxValue;
-            for (int i = 0; i < tick; i++)
-            {
-                pp--;
-            }
-            s.Stop();
-            Console.WriteLine(s.ElapsedMilliseconds);
-            Console.ReadKey();
         }
     }
 }
