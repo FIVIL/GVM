96d17a7aa7a8fa3b17006f98e3edeb1b16237d53
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-11-09 07:39:25 +0330

    :/

--- HTests/Program.cs
+++ HTests/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Numerics;
-
+using GVM.Src;
+using GVM.Src.Types;
 
 namespace HTests
 {
@@ -9,7 +10,26 @@ namespace HTests
     {
         static void Main(string[] args)
         {
-            
+            var tick = 1000_000;
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            var p = new BigInteger(Int256.MaxValue.ToByteArray());
+            for (int i = 0; i < tick; i++)
+            {
+                p--;
+            }
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
+            s.Reset();
+            s.Start();
+            Int256 pp = Int256.MaxValue;
+            for (int i = 0; i < tick; i++)
+            {
+                pp--;
+            }
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
+            Console.ReadKey();
         }
     }
 }
