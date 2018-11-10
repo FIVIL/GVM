40730acf5daabbc91b84ab80eb80ee1a13279d16
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-11-10 16:44:45 +0330

    storage

---
+++ GVM/Src/Components/Storage.cs
@@ -0,0 +1,19 @@
+﻿using GDIC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GVM.Src.Components
+{
+    public class Storage
+    {
+        private readonly Dictionary<Registers, object> data;
+        private readonly ServiceCollection services;
+        public Storage(ServiceCollection service)
+        {
+            data = new Dictionary<Registers, object>();
+            services = service;
+        }
+
+    }
+}
